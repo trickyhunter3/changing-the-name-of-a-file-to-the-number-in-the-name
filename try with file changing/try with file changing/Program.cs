16aca06af67e883c974a4d53d3fe9f08f097f58f
denisplay <denisplay12@gmail.com>
2020-10-13 17:13:50 +0300

    update 0.9
    removed more bugs from interface and add more interface

--- try with file changing/try with file changing/Program.cs
+++ try with file changing/try with file changing/Program.cs
@@ -9,23 +9,40 @@ namespace try_with_file_changing
     {
         static void Main()
         {
-            string file_type;
-            Program pg = new Program();
-            Console.WriteLine("640p 720p 1080p 2160p if the file has different resolution on the name\nthen change a bit the source code so it will filter it\n");
-            Console.Write("Enter a Path: ");
-            string path = Console.ReadLine();
-            if (path[path.Length - 1] != '\\')
-                path += '\\';
-            DirectoryInfo d = new DirectoryInfo(path);
-            FileInfo[] infos = d.GetFiles();
-            //get all the file info from the folder
-            foreach (FileInfo f in infos)
+            try
             {
-                string[] splitedbydotes = f.FullName.Split('.');
-                file_type = '.' + splitedbydotes[splitedbydotes.Length -1];
-                string num = pg.GetNumberOutOfString(f.Name, file_type).ToString();
-                if (f.FullName != path + num.ToString() + file_type)
+                int i = 1;
+                string file_type;
+                Program pg = new Program();
+                Console.WriteLine("640p 720p 1080p 2160p if the file has different resolution on the name\n" +
+                    "then change a bit the source code so it will filter it\n");
+                START:;
+                Console.Write("Enter a Path: ");
+                string path = Console.ReadLine();
+                if (path == "")
+                {
+                    goto START;
+                }                    
+                if (path[path.Length - 1] != '\\')
+                    path += '\\';
+                DirectoryInfo d = new DirectoryInfo(path);
+                FileInfo[] infos = d.GetFiles();
+                //get all the file info from the folder
+                foreach (FileInfo f in infos)
+                {
+                    string[] splitedbydotes = f.FullName.Split('.');
+                    file_type = '.' + splitedbydotes[splitedbydotes.Length - 1];
+                    string num = pg.GetNumberOutOfString(f.Name, file_type).ToString();
                     File.Move(f.FullName, path + num + file_type);
+                    Console.WriteLine(i + " Complete");
+                    i++;
+                }
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Directory not found");
+                Console.ReadLine();
             }
         }
         public int GetNumberOutOfString(string File_name, string file_type)
@@ -41,7 +58,7 @@ namespace try_with_file_changing
             int number_holder = -1;
             //hold a number if it's the only number then 
             string numbers = null;
-            for (int j = 0; j < File_name.Length; j++) 
+            for (int j = 0; j < File_name.Length; j++)
             {
                 switch (File_name[j])
                 {
@@ -60,7 +77,7 @@ namespace try_with_file_changing
                         //start recording the numbers if they are found
                         break;
                     default:
-                        if(numbers_together != 0)
+                        if (numbers_together != 0)
                         {
                             if (numbers + file_type == File_name)
                             {
@@ -69,28 +86,28 @@ namespace try_with_file_changing
                                 //if file is just a number then returns that number
                             }
                             switch (numbers)
-                                {
-                                    case "1":
-                                    case "2":
-                                    case "3":
-                                    case "4":
-                                    case "5":
-                                    case "6":
-                                    case "7":
-                                    case "8":
-                                    case "9":
-                                    case "640":
-                                    case "720":
-                                    case "1080":
-                                    case "2160":
-                                        number_holder = Convert.ToInt32(numbers);
-                                        i--;
-                                        goto END;
-                                }
+                            {
+                                case "1":
+                                case "2":
+                                case "3":
+                                case "4":
+                                case "5":
+                                case "6":
+                                case "7":
+                                case "8":
+                                case "9":
+                                case "640":
+                                case "720":
+                                case "1080":
+                                case "2160":
+                                    number_holder = Convert.ToInt32(numbers);
+                                    i--;
+                                    goto END;
+                            }
                             converted = Convert.ToInt32(numbers);
                         END:;
-                                numbers = null;
-                                i++;
+                            numbers = null;
+                            i++;
                         }
                         numbers_together = 0;
                         break;
